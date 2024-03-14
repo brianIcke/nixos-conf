fdf216fabae01a9bfcef75aa6fb34199c10b3bce
Brian Nickel <mail@brian-nickel.de>
2024-03-14 14:46:52 +0100

    added fonts

--- system/BrianTUX/configuration.nix
+++ system/BrianTUX/configuration.nix
@@ -92,6 +92,10 @@
   # Fonts
   fonts.packages = with pkgs; [
   font-awesome
+  ubuntu_font_family
+  carlito
+  vistafonts
+  corefonts
   powerline-fonts
   powerline-symbols
   (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
