e45d40351bc85e07c7db7d2fff75a2a7f366b9d6
Brian Nickel <mail@brian-nickel.de>
2024-03-27 17:29:47 +0100

    added fonts

--- system/tuxbook/configuration.nix
+++ system/tuxbook/configuration.nix
@@ -76,6 +76,10 @@
   # Fonts
   fonts.packages = with pkgs; [
   font-awesome
+  ubuntu_font_family
+  carlito
+  vistafonts
+  corefonts
   powerline-fonts
   powerline-symbols
   (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
