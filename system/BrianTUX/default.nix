74533700086bf1d631fd67442761cbbba21c64fc
Brian Nickel <mail@brian-nickel.de>
2024-08-24 23:21:49 +0200

    added brian to networkmanager group

--- system/BrianTUX/default.nix
+++ system/BrianTUX/default.nix
@@ -32,6 +32,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  users.extraGroups.networkmanager.members = [ "brian" ];
 
   # Firewall
   networking.firewall.allowedTCPPorts = [ 8080 1883 ];
